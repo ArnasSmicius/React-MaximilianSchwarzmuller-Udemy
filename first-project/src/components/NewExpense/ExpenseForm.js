a01c935914f4e11656ab976aa7e595e700af04fd
Henrikas Arnas Smičius <henrikas.smicius@vinted.com>
2023-03-23 21:02:46 +0200

    Add two-way binding to ExpenseForm

--- first-project/src/components/NewExpense/ExpenseForm.js
+++ first-project/src/components/NewExpense/ExpenseForm.js
@@ -29,6 +29,10 @@ const ExpenseForm = () => {
     }
 
     console.log(expenseData);
+
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -38,6 +42,7 @@ const ExpenseForm = () => {
           <label>Title</label>
           <input
             type="text"
+            value={enteredTitle}
             onChange={titleChangeHandler}
           />
         </div>
@@ -47,6 +52,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -54,6 +60,7 @@ const ExpenseForm = () => {
           <label>Date</label>
           <input
             type="date"
+            value={enteredDate}
             min="2019-01-01"
             max="2023-12-31"
             onChange={dateChangeHandler}
